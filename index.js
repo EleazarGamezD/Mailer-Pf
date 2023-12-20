1afb22a4d215bce034c3b81d2661406e17a450b8
Eleazar Gamez <82742317+EleazarGamezD@users.noreply.github.com>
2023-12-20 03:12:18 -0500

    add readme and delete some lines of code not finish

--- index.js
+++ index.js
@@ -1,69 +1,43 @@
-import express from 'express';
-import path from 'path'; //importando modulo de path "directorios"
-import { Resend } from 'resend';
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'; // importando modulo de lectura de archivo ENV variables de entornos
-import url from 'url';
+import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv"; // importando modulo de lectura de archivo ENV variables de entornos
 
-dotenv.config (); // este comando llama al archivo de variable de entorno.
-const app = express ();
+dotenv.config(); // este comando llama al archivo de variable de entorno.
 
-const resend = new Resend(process.env.Resend_API_KEY);
+const app = express();
+
 const appPort = process.env.APP_PORT || 3000;
-console.log(process.env.Resend_API_KEY);
-
 
 const transporter = nodemailer.createTransport({
   host: "smtp.forwardemail.net",
-  service: 'gmail',
+  service: "gmail",
   port: 465,
   secure: true,
-  auth: {   
-    user:  process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASSWORD,
-  },
-});
-
-
-
-
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-app.use(express.static(path.join(__dirname,)));
-app.use(express.json());
-
- app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-}); 
-
-
-app.get ('/send', async (req, res,next) => {
-  console.log(req);
-  const { subject, html } = req.body;
-  const msg = {
-    from: process.env.FROM,
-    to:   process.env.TO,
-    subject,
-    text: "Hello world?",
-    html,
-   
-  };
-
-  try {
-   
-    const data = await transporter.sendMail(msg);
-    console.log('data es',{data})
-    res.status(200).json(data);
-  } catch (error) {
-    
-    const messages = error
-    res.status (400).send (messages);
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD
   }
 });
 
+app.get("/send", async (req, res, next) => {
+  const { subject, text, html } = req.body;
+  const msg = {
+    from: process.env.FROM,
+    to: process.env.TO,
+    subject,
+    text,
+    html
+  };
 
+  try {
+    const data = await transporter.sendMail(msg);
+    res.status(200).json(data);
+  } catch (error) {
+    const messages = error;
+    res.status(400).send(messages);
+  }
+});
 
-app.listen(appPort, () => console.log(`ejecutando la app en el puerto ${ appPort }`));
+app.listen(appPort, () =>
+  console.log(`ejecutando la app en el puerto ${appPort}`)
+);
